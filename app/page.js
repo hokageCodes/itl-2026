3fd0741afba896864e6952f5fe4aa93934456e82
hokage_sama <hashsenju94@gmail.com>
2025-11-17 13:50:02 +0000

    itl-2026

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import HeroSection from '@/components/sections/HeroSection';
 import ConferenceSection from '@/components/sections/ConferenceSection';
 import BenefitsSection from '@/components/sections/BenefitsSection';
 import SpeakersSection from '@/components/sections/SpeakersSection';
+import SponsorsSection from '@/components/sections/SponsorsSection';
 import FAQsSection from '@/components/sections/FAQsSection';
 
 export default function Home() {
